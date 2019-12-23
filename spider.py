b936b491e0eaf87029386a65c656dbe6e1f5089f
Adriaan Knapen <hi@aknapen.nl>
2019-12-23 13:45:09 +0100

    :sparkles: Adapt spider for book.be

--- spider.py
+++ spider.py
@@ -1,14 +1,14 @@
 import scrapy
 
-QUERY = "q=kw%3A*&fq=%28%28x0%3Abook+x4%3Aprintbook%29%29+>+yr%3A2019+>++>+ln%3Adut+>+fm%3Afic&dblist=638"
+URL = "https://www.boek.be/zoek/boek/?e=1&genre=&archief=0&productvorm=papierenboeken%2Cebooks%2Cluisterboeken%2Coverigeboeken"
 
 class Spider(scrapy.Spider):
-    name = 'worldcatspider'
-    start_urls = ['https://www.worldcat.org/search?start=%s&%s' % (number, QUERY) for number in range(0, 4400, 10)]
+    name = 'boek-be'
+    start_urls = [URL]
 
     def parse(self, response):
-        for title in response.css('.name a > strong ::text').extract():
+        for title in response.css('.title ::text').extract():
             yield {"title:": title}
 
-        # for next_page in response.css('a.next-posts-link'):
-        #    yield response.follow(next_page, self.parse)
+        for next_page in response.css('.item-list-pager a'):
+           yield response.follow(next_page, self.parse)
